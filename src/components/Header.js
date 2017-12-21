7c4d8661e4abc6d1c4c1104e1effd18f517cdb02
mpochiro <mpochiro7@gmail.com>
2017-12-20 21:04:15 -0700

    testing button

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,8 @@ const Header = () => (
     <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create Topic</NavLink>
     <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-    <button onClick={startLogout}><a href='https://whispering-reef-72580.herokuapp.com/'>Logout</a></button>
+    <button onClick={startLogout}Logout</button>
+    <button>hello testing</button>
   </header>
 );
 
